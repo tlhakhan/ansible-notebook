593952bc70b46dc57ff7745211289aabe4ab7587
tlhakhang <tenzin.lhakhang@gmail.com>
2017-04-05 21:41:26 -0400

    better mysql deployment

--- freshen.sh
+++ freshen.sh
@@ -2,3 +2,4 @@
 
 BASEDIR=$(dirname $BASH_SOURCE)
 find $BASEDIR -name *.retry | xargs -n1 rm -f
+find $BASEDIR -name *.log | xargs -n1 rm -f
--- mysql/roles/service-mysql/rpms/get_rpms.sh
+++ mysql/roles/service-mysql/rpms/get_rpms.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $BASH_SOURCE)
-MYSQL_VERSION=5.6
 
-# get rpms
-cp -Rav /opt/software/mysql/$MYSQL_VERSION/*.rpm $BASEDIR 
+# mysql catalog
+MYSQL_VERSIONS=( 5.6.35 5.7.17 )
+MYSQL_VERSION_URLS=( "https://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-5.6.35-1.el7.x86_64.rpm-bundle.tar" "https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.17-1.el7.x86_64.rpm-bundle.tar" )
+
+# get rpm bundle
+for i in ${!MYSQL_VERSIONS[@]}
+do
+    # makes sure that file rpm bundle tar doesn't already exist, otherwise download bundle
+    # make sure that the version folder exists, otherwise create
+    # untar the bundle into respective version folder
+    [[ -e $BASEDIR/$(basename ${MYSQL_VERSION_URLS[i]}) ]] || curl -s -LO ${MYSQL_VERSION_URLS[i]}
+    [[ -d $BASEDIR/${MYSQL_VERSIONS[i]} ]] || mkdir $BASEDIR/${MYSQL_VERSIONS[i]}
+    tar -xvf $(basename ${MYSQL_VERSION_URLS[i]}) -C $BASEDIR/${MYSQL_VERSIONS[i]}
+done
